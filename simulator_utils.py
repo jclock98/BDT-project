a946abe1ae475452d326d59efe860c1b7e5bb75d
Jacopo Clocchiatti <jacopo.clocchiatti@gmail.com>
2022-07-28 08:38:36 +0200

    add script to populate the db

---
+++ simulator_utils.py
@@ -0,0 +1,11 @@
+class FitnessTracker():
+    def __init__(self):
+        self.location = ""
+        self.start_time = ""
+        self.type = ""
+        
+class FacilityAccess():
+    def __init__(self):
+        self.facility = ""
+        self.location = ""
+        self.start_time = ""
--- test.py
+++ test.py
@@ -1,108 +1,83 @@
-import multiprocessing as mp
 import osmnx as ox
 from time import time
 import concurrent.futures
+import psycopg2 as psypg
 
-def get_amenities(territory):
-    amenity_tags = {
-        'amenity':[
-            'dive_centre',
-            'bicycle_parking',
-            'bicycle_rental',
-        ]
-    }
-    start_time = time()
-    print("Doing amenities...")
-    amenities = ox.geometries_from_place(territory, tags=amenity_tags)
-    print(f"Amenities done in {time()-start_time}s!")
-    print(f"Amenities results => {amenities.shape}")
 
-def get_buildings(territory):
-    buildings_tags = {
-        'buildings':[
-            'grandstand',
-            'pavillion',
-            'riding_hall',
-            'sports_hall',
-            'stadium'
-        ]
-    }
-    start_time = time()
-    print("Doing buildings...")
-    buildings = ox.geometries_from_place(territory, tags=buildings_tags)
-    print(f"Buildings done in {time()-start_time}s!")
-    
-    print(f"Buildings results => {buildings.shape}")
-
-def get_highways(territory):
-    highway_tags = {
-        'highway':[
-            'path',
-            'footway',
-            'cycleway',
-            'bridleway',
-            'track',
-        ]
-    }
-    start_time = time()
-    print("Doing highways...")
-    highways = ox.geometries_from_place(territory, tags=highway_tags)
-    print(f"Higways done in {time()-start_time}s!")
-    
-    print(f"Highways results => {highways.shape}")
-
-def get_leisures(territory):
+def get_leisures(municipality):
+    istat = municipality[0]
+    city = municipality[1]
+    province = municipality[2]
+    region = municipality[3]
     leisure_tags = {
         'leisure':[
             'fitness_centre',
             'sport_centre',
-            'dance',
-            'disc_golf_course',
-            'fitness_station',
-            'horse_riding',
             'ice_rink',
-            'pitch',
             'stadium',
             'swimming_area',
             'swimming_pool',
-            'track'
         ]
     }
+    address = f"{city}, {province}, {region}, Italy"
     start_time = time()
-    print("Doing leisures...")
-    leisures = ox.geometries_from_place(territory, tags=leisure_tags)
+    print(f"Doing leisures for {city}...")
+    try:
+        leisures = ox.geometries_from_place(address, tags=leisure_tags)
+    except:
+        return
     print(f"Leisures done in {time()-start_time}s!")
-    
     print(f"Leisures results => {leisures.shape}")
+    if leisures.shape[0] > 0:
+        try:
+            conn = psypg.connect(
+            host=HOST,
+            database=DBNAME,
+            user=USER,
+            password=PSWD)
+            
+            cur = conn.cursor()
     
-def get_routes(territory):
-    route_tags = {
-        'route':[
-            'bicycle',
-            'canoe',
-            'hiking',
-            'inline_skates',
-            'mtb',
-            'piste',
-            'running',
-        ]
-    }
-    start_time = time()
-    print("Doing routes...")
-    routes = ox.geometries_from_place(territory, tags=route_tags)
-    print(f"Routes done in {time()-start_time}s!")
-    
-    print(f"Routes results => {routes.shape}")
-
-def exec_func(arguments):
-    f,arg = arguments
-    f(arg)
+            sql = "INSERT INTO places(name, typology, municipality) VALUES"
+            args_str = ','.join(cur.mogrify("(%s,%s,%s)",
+                                    (leisure.name if hasattr(leisure, "name") else None,
+                                     leisure.leisure,
+                                     istat)).decode("utf-8") 
+                                for leisure in leisures.itertuples())
+            cur.execute(sql+args_str)
+            # commit the changes to the database
+            conn.commit()
+            # close communication with the database
+            cur.close()
+        except (Exception, psypg.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
 
 if __name__ == '__main__':
-    italy_region = ["Valle D'Aosta", "Piemonte", "Liguria", "Lombardia", "Veneto", "Friuli-Venezia Giulia", "Trentino-Alto Adige", "Emilia-Romagna", "Toscana", "Lazio", "Umbria", "Marche", "Abruzzo", "Molise", "Campania", "Puglia", "Basilicata", "Calabria", "Sardegna", "Sicilia"]
-    for region in italy_region:
-        print(f"Starting {region}")
-        list_func = [get_amenities, get_buildings, get_highways, get_leisures, get_routes]
-        with concurrent.futures.ProcessPoolExecutor(len(list_func)) as pool:
-            results = list(pool.map(exec_func, zip(list_func, [region for _ in range(len(list_func))])))
-            print(results)
+    try:
+        conn = psypg.connect(
+            host=HOST,
+            database=DBNAME,
+            user=USER,
+            password=PSWD)
+        
+        cur = conn.cursor()
+        
+        sql = """SELECT istat, municipality, m.province, region 
+                FROM municipalities AS m JOIN provinces AS p 
+                ON m.province=p.province"""
+        cur.execute(sql)
+        # commit the changes to the database
+        municipalities = cur.fetchall()
+        # close communication with the database
+        cur.close()
+    except (Exception, psypg.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            
+    with concurrent.futures.ProcessPoolExecutor(5) as pool:
+        results = list(pool.map(get_leisures, municipalities))
